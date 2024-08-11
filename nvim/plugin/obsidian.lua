e1616125ecddc983fb4cfed657291741720a90bd
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-08-11 22:06:02 +0200

    impr: Minor saving-changes

--- nvim/plugin/obsidian.lua
+++ nvim/plugin/obsidian.lua
@@ -81,17 +81,7 @@ require("obsidian").setup({
         -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
         -- In this case a note with the title 'My new note' will be given an ID that looks
         -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-        local suffix = ""
-        if title ~= nil then
-            -- If title is given, transform it into valid file name.
-            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-        else
-            -- If title is nil, just add 4 random uppercase letters to the suffix.
-            for _ = 1, 4 do
-                suffix = suffix .. string.char(math.random(65, 90))
-            end
-        end
-        return tostring(os.time()) .. "-" .. suffix
+        return title
     end,
 
     -- Optional, customize how note file names are generated given the ID, target directory, and title.
@@ -135,22 +125,7 @@ require("obsidian").setup({
     -- Optional, alternatively you can customize the frontmatter data.
     ---@return table
     note_frontmatter_func = function(note)
-        -- Add the title of the note as an alias.
-        if note.title then
-            note:add_alias(note.title)
-        end
-
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-            for k, v in pairs(note.metadata) do
-                out[k] = v
-            end
-        end
-
-        return out
+        return {}
     end,
 
     -- Optional, for templates (see below).

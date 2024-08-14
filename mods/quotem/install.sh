6890ce03e3ff0a3b7d26f09bc3221852f2cb44f3
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-08-14 19:28:28 +0200

    impr: Minor restructure

--- mods/quotem/install.sh
+++ mods/quotem/install.sh
@@ -2,4 +2,5 @@
 # Change directory to project-directory.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-git clone --branch main --depth 1 https://github.com/dennis-n-schneider/quotem.git && cd quotem && make
+[ -d quotem ] || (git clone --branch main --depth 1 https://github.com/dennis-n-schneider/quotem.git
+cd quotem && make

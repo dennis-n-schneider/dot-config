cc93bea84e4f90c0a1defd23b4d6df81a0fcf466
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-08-14 16:50:13 +0200

    feat: Initial integration of quotem

---
+++ mods/quotem/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Change directory to project-directory.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+git clone --branch main --depth 1 https://github.com/dennis-n-schneider/quotem.git && cd quotem && make

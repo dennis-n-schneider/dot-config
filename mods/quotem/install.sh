425f0d4e83148af923bd2ed4f500cc76b9a1cb08
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-08-14 21:05:09 +0200

    fix: Errors in mod installation scripts

--- mods/quotem/install.sh
+++ mods/quotem/install.sh
@@ -2,5 +2,5 @@
 # Change directory to project-directory.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-[ -d quotem ] || (git clone --branch main --depth 1 https://github.com/dennis-n-schneider/quotem.git
+[ -d quotem ] || git clone --branch main --depth 1 https://github.com/dennis-n-schneider/quotem.git
 cd quotem && make
--- mods/workbenches/install.sh
+++ mods/workbenches/install.sh
@@ -6,7 +6,7 @@ QTILE_CONFIG_DIR="$XDG_CONFIG_HOME/qtile"
 [ -d $TMP_DIR ] || mkdir $TMP_DIR
 
 # Installs the workbenches-extension for qtile.
-[ -d qtile-workbenches ] || git clone --branch master --depth 1 https://github.com/dennis-n-schneider/qtile-workbenches.git
+[ -d qtile-workbenches ] || git clone --branch main --depth 1 https://github.com/dennis-n-schneider/qtile-workbenches.git
 
 rsync -av qtile-workbenches/qt-workbenches/*.py $QTILE_CONFIG_DIR
 

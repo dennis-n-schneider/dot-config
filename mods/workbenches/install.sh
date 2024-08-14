06bd7e1ed291b9e75d8e1732a0beee8424a6b1aa
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-08-14 19:24:11 +0200

    impr: Pull workbenches from Github

--- mods/workbenches/install.sh
+++ mods/workbenches/install.sh
@@ -2,16 +2,13 @@
 # Change directory to project-directory.
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-TMP_DIR="./.tmp"
-QTILE_CONFIG_DIR="$HOME/.config/qtile"
+QTILE_CONFIG_DIR="$XDG_CONFIG_HOME/qtile"
 [ -d $TMP_DIR ] || mkdir $TMP_DIR
 
 # Installs the workbenches-extension for qtile.
-wget --content-disposition -P $TMP_DIR "https://raw.githubusercontent.com/dennis-n-schneider/qtile-workbenches/main/qt-workbenches/core.py"
-wget --content-disposition -P $TMP_DIR "https://raw.githubusercontent.com/dennis-n-schneider/qtile-workbenches/main/qt-workbenches/workbenches.py"
+[ -d qtile-workbenches ] || git clone --branch master --depth 1 https://github.com/dennis-n-schneider/qtile-workbenches.git
 
-rsync -av $TMP_DIR/* $QTILE_CONFIG_DIR
+rsync -av qtile-workbenches/qt-workbenches/*.py $QTILE_CONFIG_DIR
 
 patch $QTILE_CONFIG_DIR/config.py ./workbenches.diff
 
-rm $TMP_DIR/*

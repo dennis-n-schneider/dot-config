22e8e601e44310f046af7fe34663e30c80a09a08
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2024-10-13 20:51:02 +0200

    fix: Does not use cache-directory for git repository download

--- mods/workbenches/install.sh
+++ mods/workbenches/install.sh
@@ -3,12 +3,13 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 QTILE_CONFIG_DIR="$XDG_CONFIG_HOME/qtile"
-[ -d $TMP_DIR ] || mkdir $TMP_DIR
+TMP_DIR="$XDG_CACHE_HOME/dot-config/qtile-workbenches"
+[ -d $TMP_DIR ] || mkdir -p $TMP_DIR
 
 # Installs the workbenches-extension for qtile.
-[ -d qtile-workbenches ] || git clone --branch main --depth 1 https://github.com/dennis-n-schneider/qtile-workbenches.git
+[ -d $TMP_DIR ] || git clone --branch main --depth 1 https://github.com/dennis-n-schneider/qtile-workbenches.git $TMP_DIR
 
-rsync -av qtile-workbenches/qt-workbenches/*.py $QTILE_CONFIG_DIR
+rsync -av $TMP_DIR/qt-workbenches/*.py $QTILE_CONFIG_DIR
 
 patch $QTILE_CONFIG_DIR/config.py ./workbenches.diff
 
